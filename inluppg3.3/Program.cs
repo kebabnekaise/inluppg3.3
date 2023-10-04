21a663ce9fbcc7fb47bf789ed640436683889f3d
emsi06001 <emsi06001@E-PW0234N3>
2023-10-04 12:30:33 +0200

    Add project files.

---
+++ inluppg3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace inluppg3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra?");
+            int ans = int.Parse(Console.ReadLine());
+
+            if (ans * 80 <= 950)
+            {
+                Console.WriteLine(ans * 80 + " kr");
+            }
+            else
+            {
+                Console.WriteLine("Du får inte hyra så länge");
+            }
+        }
+    }
+}
